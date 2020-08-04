6eb0b479009a5f01ac052f0b709cb8a46499ea11
chexiangqian <2439836687@qq.com>
2020-08-04 18:54:45 +0800

    批量暂停后继续下载进度bug修改

--- student/src/main/java/com/jkrm/education/ui/activity/CourseCacheChildActivity.java
+++ student/src/main/java/com/jkrm/education/ui/activity/CourseCacheChildActivity.java
@@ -1,20 +1,35 @@
 package com.jkrm.education.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.hzw.baselib.base.AwBaseActivity;
 import com.hzw.baselib.util.AwRecyclerViewUtil;
+import com.hzw.baselib.util.FileUtils;
 import com.hzw.baselib.widgets.AwViewCustomToolbar;
 import com.jkrm.education.R;
 import com.jkrm.education.adapter.CourseCacheChildAdapter;
+import com.jkrm.education.bean.rx.RxCostomDownType;
 import com.jkrm.education.constants.Extras;
 import com.jkrm.education.db.DaoCatalogueBean;
 import com.jkrm.education.db.DaoVideoBean;
 import com.jkrm.education.db.util.DaoUtil;
+import com.jkrm.education.download.DownloadLimitManager;
+import com.jkrm.education.download.DownloadThreadManager;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -30,7 +45,13 @@ public class CourseCacheChildActivity extends AwBaseActivity {
     TextView mTvPauseAll;
     @BindView(R.id.rcv_data)
     RecyclerView mRcvData;
-    private String MICROLESS_ID,MICROLESS_NAME;
+    @BindView(R.id.cb_all)
+    CheckBox mCbAll;
+    @BindView(R.id.btn_delete)
+    Button mBtnDelete;
+    @BindView(R.id.ll_of_setting)
+    LinearLayout mLlOfSetting;
+    private String MICROLESS_ID, MICROLESS_NAME;
     private CourseCacheChildAdapter mCourseCacheChildAdapter;
     private CopyOnWriteArrayList<DaoVideoBean> mDaoVideoBeans = new CopyOnWriteArrayList<DaoVideoBean>();
 
@@ -68,16 +89,103 @@ public class CourseCacheChildActivity extends AwBaseActivity {
         mToolbarCustom.setOnRightClickListener(new AwViewCustomToolbar.OnRightClickListener() {
             @Override
             public void onRightTextClick() {
-                if("编辑".equals(mToolbarCustom.getRightText())){
+                if ("编辑".equals(mToolbarCustom.getRightText())) {
                     mToolbarCustom.setRightText("完成");
                     mCourseCacheChildAdapter.setChose(true);
-                }else if("完成".equals(mToolbarCustom.getRightText())){
+                    showView(mLlOfSetting, true);
+                } else if ("完成".equals(mToolbarCustom.getRightText())) {
                     mToolbarCustom.setRightText("编辑");
                     mCourseCacheChildAdapter.setChose(false);
+                    showView(mLlOfSetting, false);
                 }
                 mCourseCacheChildAdapter.notifyDataSetChanged();
 
             }
         });
+        mCourseCacheChildAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                DaoVideoBean daoVideoBean = (DaoVideoBean) adapter.getData().get(position);
+                toClass(StudyCourseActivity.class, false, Extras.MICROLESS_ID, MICROLESS_ID, Extras.FILE_NAME, daoVideoBean.getFilePath());
+            }
+        });
+        mCbAll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                for (DaoVideoBean daoVideoBean : mDaoVideoBeans) {
+                    daoVideoBean.setIsCheck(b);
+                }
+                mCourseCacheChildAdapter.notifyDataSetChanged();
+            }
+        });
+        mBtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<DaoVideoBean> daoVideoBeans = new ArrayList<>();
+                for (DaoVideoBean daoVideoBean : mDaoVideoBeans) {
+                    if (daoVideoBean.getIsCheck()) {
+                        daoVideoBeans.add(daoVideoBean);
+                        DownloadLimitManager.getInstance().cancelDownload(daoVideoBean);//取消下载
+                        DaoUtil.getInstance().deleteVideoBean(daoVideoBean);//删除数据库信息
+                        FileUtils.deleteSingleFile(getApplicationContext(), daoVideoBean.getFilePath());//删除文件
+                    }
+                }
+                mDaoVideoBeans.removeAll(daoVideoBeans);
+                mCourseCacheChildAdapter.setChose(false);
+                mCourseCacheChildAdapter.notifyDataSetChanged();
+                mToolbarCustom.setRightText("编辑");
+                showView(mLlOfSetting, false);
+            }
+        });
+        mTvPauseAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("全部暂停".equals(mTvPauseAll.getText().toString())) {
+                    DownloadThreadManager.getInstance().removeDownList(mDaoVideoBeans);
+                    //更新状态
+                    ArrayList<DaoVideoBean> needPauseVideos = new ArrayList<>();
+                    for (DaoVideoBean daoVideoBean : mDaoVideoBeans) {
+                        if (!DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                            needPauseVideos.add(daoVideoBean);
+                            daoVideoBean.setDownloadStatus(DaoVideoBean.DOWNLOAD_PAUSE);
+                            EventBus.getDefault().post(daoVideoBean);
+                        }
+                    }
+                    mTvPauseAll.setText("全部开始");
+                    Drawable drawable = getResources().getDrawable(R.mipmap.load_start_blue);
+                    mTvPauseAll.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+                } else if ("全部开始".equals(mTvPauseAll.getText().toString())) {
+                    for (DaoVideoBean daoVideoBean : mDaoVideoBeans) {
+                        if (!DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                            daoVideoBean.setDownloadStatus(DaoVideoBean.DOWNLOAD);
+                            EventBus.getDefault().post(daoVideoBean);
+                        }
+                    }
+                    EventBus.getDefault().post(new RxCostomDownType(mDaoVideoBeans));
+                    mTvPauseAll.setText("全部暂停");
+                    Drawable drawable = getResources().getDrawable(R.mipmap.load_pause_blue);
+                    mTvPauseAll.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+                }
+            }
+        });
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void update(DaoVideoBean info) {
+        if (DaoVideoBean.DOWNLOAD.equals(info.getDownloadStatus())) {
+            mCourseCacheChildAdapter.updateProgress(info);
+        } else if (DaoVideoBean.DOWNLOAD_OVER.equals(info.getDownloadStatus())) {
+            mCourseCacheChildAdapter.updateProgress(info);
+        } else if (DaoVideoBean.DOWNLOAD_PAUSE.equals(info.getDownloadStatus())) {
+            mCourseCacheChildAdapter.updateProgress(info);
+            // Toast.makeText(this,"下载暂停",Toast.LENGTH_SHORT).show();
+        } else if (DaoVideoBean.DOWNLOAD_CANCEL.equals(info.getDownloadStatus())) {
+            mCourseCacheChildAdapter.updateProgress(info);
+            DaoUtil.getInstance().deleteVideoBean(info);
+        } else if (DaoVideoBean.DOWNLOAD_WAIT.equals(info.getDownloadStatus())) {
+            mCourseCacheChildAdapter.updateProgress(info);
+        } else if (DaoVideoBean.DOWNLOAD_ERROR.equals(info.getDownloadStatus())) {
+        }
     }
 }
--- student/src/main/java/com/jkrm/education/ui/activity/CourseCacheNewActivity.java
+++ student/src/main/java/com/jkrm/education/ui/activity/CourseCacheNewActivity.java
@@ -1,22 +1,32 @@
 package com.jkrm.education.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.hzw.baselib.base.AwBaseActivity;
 import com.hzw.baselib.util.AwRecyclerViewUtil;
+import com.hzw.baselib.util.FileUtils;
 import com.hzw.baselib.widgets.AwViewCustomToolbar;
 import com.jkrm.education.R;
 import com.jkrm.education.adapter.CourseCacheAdapter;
+import com.jkrm.education.bean.rx.RxCostomDownType;
 import com.jkrm.education.constants.Extras;
 import com.jkrm.education.db.DaoCatalogueBean;
 import com.jkrm.education.db.DaoMicroLessonBean;
 import com.jkrm.education.db.DaoVideoBean;
 import com.jkrm.education.db.util.DaoUtil;
+import com.jkrm.education.download.DownloadLimitManager;
+import com.jkrm.education.download.DownloadThreadManager;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -41,6 +51,12 @@ public class CourseCacheNewActivity extends AwBaseActivity {
     TextView mTvPauseAll;
     @BindView(R.id.rcv_data)
     RecyclerView mRcvData;
+    @BindView(R.id.cb_all)
+    CheckBox mCbAll;
+    @BindView(R.id.btn_delete)
+    Button mBtnDelete;
+    @BindView(R.id.ll_of_setting)
+    LinearLayout mLlOfSetting;
     private List<DaoMicroLessonBean> mDaoMicroLessonBeans = new ArrayList<>();
     private Set<DaoMicroLessonBean> microLessonBeanSet = new HashSet<>();
     private CourseCacheAdapter mCourseCacheAdapter;
@@ -59,8 +75,8 @@ public class CourseCacheNewActivity extends AwBaseActivity {
 
             }
         });
-        mCourseCacheAdapter=new CourseCacheAdapter();
-        AwRecyclerViewUtil.setRecyclerViewLinearlayout(mActivity, mRcvData,mCourseCacheAdapter,false);
+        mCourseCacheAdapter = new CourseCacheAdapter();
+        AwRecyclerViewUtil.setRecyclerViewLinearlayout(mActivity, mRcvData, mCourseCacheAdapter, false);
     }
 
     @Override
@@ -88,21 +104,217 @@ public class CourseCacheNewActivity extends AwBaseActivity {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
                 List<DaoMicroLessonBean> data = adapter.getData();
-                toClass(CourseCacheChildActivity.class,false, Extras.MICROLESS_ID,data.get(position).getId(),Extras.MICROLESS_NAME,data.get(position).getMlessonName());
+                toClass(CourseCacheChildActivity.class, false, Extras.MICROLESS_ID, data.get(position).getId(), Extras.MICROLESS_NAME, data.get(position).getMlessonName());
             }
         });
         mToolbarCustom.setOnRightClickListener(new AwViewCustomToolbar.OnRightClickListener() {
             @Override
             public void onRightTextClick() {
-                if("编辑".equals(mToolbarCustom.getRightText())){
+                if ("编辑".equals(mToolbarCustom.getRightText())) {
                     mToolbarCustom.setRightText("完成");
                     mCourseCacheAdapter.setChose(true);
-                }else if("完成".equals(mToolbarCustom.getRightText())){
+                    showView(mLlOfSetting, true);
+                } else if ("完成".equals(mToolbarCustom.getRightText())) {
                     mToolbarCustom.setRightText("编辑");
                     mCourseCacheAdapter.setChose(false);
+                    showView(mLlOfSetting, false);
                 }
                 mCourseCacheAdapter.notifyDataSetChanged();
             }
         });
+        mCbAll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                for (DaoMicroLessonBean daoMicroLessonBean : mDaoMicroLessonBeans) {
+                    daoMicroLessonBean.setIsCheck(b);
+                }
+                mCourseCacheAdapter.notifyDataSetChanged();
+            }
+        });
+        mBtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<DaoMicroLessonBean> list = new ArrayList<>();
+                for (DaoMicroLessonBean daoMicroLessonBean : mDaoMicroLessonBeans) {
+                    if (daoMicroLessonBean.getIsCheck()) {
+                        list.add(daoMicroLessonBean);
+                    }
+                }
+                if (list.isEmpty()) {
+                    showMsg("请选择课程");
+                    return;
+                }
+                for (DaoMicroLessonBean daoMicroLessonBean : list) {
+                    List<DaoCatalogueBean> daoCatalogueBeans = DaoUtil.getInstance().queryCatalogueListByQueryBuilder(daoMicroLessonBean.getId());
+                    for (DaoCatalogueBean daoCatalogueBean : daoCatalogueBeans) {
+                        List<DaoVideoBean> daoVideoBeans1 = DaoUtil.getInstance().queryVideoListByQueryBuilder(daoCatalogueBean.getId());
+                        for (DaoVideoBean daoVideoBean : daoVideoBeans1) {
+                            DownloadLimitManager.getInstance().cancelDownload(daoVideoBean);
+                            FileUtils.deleteSingleFile(mActivity, daoVideoBean.getFilePath());
+                        }
+                        DaoUtil.getInstance().deleteVideoList(daoVideoBeans1);
+                    }
+                }
+                mDaoMicroLessonBeans.removeAll(list);
+                mCourseCacheAdapter.setChose(false);
+                mCourseCacheAdapter.notifyDataSetChanged();
+                mToolbarCustom.setRightText("编辑");
+                showView(mLlOfSetting, false);
+            }
+        });
+        mTvPauseAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("全部暂停".equals(mTvPauseAll.getText().toString())) {
+                    pauseAllDownload();
+                    mTvPauseAll.setText("全部开始");
+                    Drawable drawable = getResources().getDrawable(R.mipmap.load_start_blue);
+                    mTvPauseAll.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+                } else if ("全部开始".equals(mTvPauseAll.getText().toString())) {
+                    continueAllDownLoad();
+                    mTvPauseAll.setText("全部暂停");
+                    Drawable drawable = getResources().getDrawable(R.mipmap.load_pause_blue);
+                    mTvPauseAll.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+                }
+            }
+        });
+    }
+
+    /**
+     * 继续下载
+     */
+    private void continueAllDownLoad() {
+        ArrayList<DaoVideoBean> needPauseVideos = new ArrayList<>();
+        for (DaoMicroLessonBean daoMicroLessonBean : mDaoMicroLessonBeans) {
+            daoMicroLessonBean.setPause(false);
+            List<DaoCatalogueBean> daoCatalogueBeans = DaoUtil.getInstance().queryCatalogueListByQueryBuilder(daoMicroLessonBean.getId());
+            for (DaoCatalogueBean daoCatalogueBean : daoCatalogueBeans) {
+                List<DaoVideoBean> daoVideoBeans1 = DaoUtil.getInstance().queryVideoListByQueryBuilder(daoCatalogueBean.getId());
+                for (DaoVideoBean daoVideoBean : daoVideoBeans1) {
+                    if (!DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                        needPauseVideos.add(daoVideoBean);
+                    }
+                }
+
+            }
+
+        }
+        for (DaoVideoBean needPauseVideo : needPauseVideos) {
+            if (!needPauseVideo.getDownloadStatus().equals(DaoVideoBean.DOWNLOAD_OVER)) {
+                needPauseVideo.setDownloadStatus(DaoVideoBean.DOWNLOAD);
+                EventBus.getDefault().post(needPauseVideo);
+                DownloadThreadManager.getInstance().downVideo(needPauseVideo);
+            }
+        }
+        mCourseCacheAdapter.notifyDataSetChanged();
+    }
+
+
+    /**
+     * 暂停全部下载
+     */
+    private void pauseAllDownload() {
+        //移除等待中的课程
+        ArrayList<DaoVideoBean> needPauseVideos = new ArrayList<>();
+        for (DaoMicroLessonBean daoMicroLessonBean : mDaoMicroLessonBeans) {
+            daoMicroLessonBean.setPause(true);
+            List<DaoCatalogueBean> daoCatalogueBeans = DaoUtil.getInstance().queryCatalogueListByQueryBuilder(daoMicroLessonBean.getId());
+            for (DaoCatalogueBean daoCatalogueBean : daoCatalogueBeans) {
+                List<DaoVideoBean> daoVideoBeans1 = DaoUtil.getInstance().queryVideoListByQueryBuilder(daoCatalogueBean.getId());
+                for (DaoVideoBean daoVideoBean : daoVideoBeans1) {
+                    if (!DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                        daoVideoBean.setDownloadStatus(DaoVideoBean.DOWNLOAD_PAUSE);
+                        needPauseVideos.add(daoVideoBean);
+                        EventBus.getDefault().post(daoVideoBean);
+                    }
+                }
+            }
+        }
+        DownloadThreadManager.getInstance().removeDownList(needPauseVideos);
+        for (DaoVideoBean needPauseVideo : needPauseVideos) {
+            if (!needPauseVideo.getDownloadStatus().equals(DaoVideoBean.DOWNLOAD_OVER)) {
+                needPauseVideo.setDownloadStatus(DaoVideoBean.DOWNLOAD_PAUSE);
+                EventBus.getDefault().post(needPauseVideo);
+            }
+        }
+        mCourseCacheAdapter.notifyDataSetChanged();
+
+    }
+
+    /**
+     * 暂停当前目录下的视频下载
+     *
+     * @param daoMicroLessonBean
+     */
+    private void pauseCatalogueDownload(DaoMicroLessonBean daoMicroLessonBean) {
+        daoMicroLessonBean.setPause(true);
+        ArrayList<DaoVideoBean> needPauseVideos = new ArrayList<>();
+        for (DaoCatalogueBean daoCatalogueBean : daoMicroLessonBean.getList()) {
+            List<DaoVideoBean> mVideoBeanList = daoCatalogueBean.getMVideoBeanList();
+            for (DaoVideoBean daoVideoBean : mVideoBeanList) {
+                if (!DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                    daoVideoBean.setDownloadStatus(DaoVideoBean.DOWNLOAD_PAUSE);
+                    needPauseVideos.add(daoVideoBean);
+                    EventBus.getDefault().post(daoVideoBean);
+                }
+            }
+        }
+        DownloadThreadManager.getInstance().removeDownList(needPauseVideos);
+        mCourseCacheAdapter.notifyDataSetChanged();
+    }
+
+
+    /**
+     * //继续下载目录下的视频
+     *
+     * @param daoMicroLessonBean
+     */
+    private void continueCatalogueDownLoad(DaoMicroLessonBean daoMicroLessonBean) {
+        daoMicroLessonBean.setPause(false);
+        ArrayList<DaoVideoBean> needPauseVideos = new ArrayList<>();
+        for (DaoCatalogueBean daoCatalogueBean : daoMicroLessonBean.getList()) {
+            List<DaoVideoBean> mVideoBeanList = daoCatalogueBean.getMVideoBeanList();
+            for (DaoVideoBean daoVideoBean : mVideoBeanList) {
+                if (!DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                    daoVideoBean.setDownloadStatus(DaoVideoBean.DOWNLOAD);
+                    needPauseVideos.add(daoVideoBean);
+                    EventBus.getDefault().post(daoVideoBean);
+                }
+            }
+        }
+        for (DaoVideoBean needPauseVideo : needPauseVideos) {
+            if (!needPauseVideo.getDownloadStatus().equals(DaoVideoBean.DOWNLOAD_OVER)) {
+                needPauseVideo.setDownloadStatus(DaoVideoBean.DOWNLOAD);
+                EventBus.getDefault().post(needPauseVideo);
+                DownloadThreadManager.getInstance().downVideo(needPauseVideo);
+            }
+        }
+        mCourseCacheAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * 获取文件大小
+     */
+    private void setCourseNumAndSize() {
+        for (DaoMicroLessonBean daoMicroLessonBean : mDaoMicroLessonBeans) {
+            int cacheNum = 0;
+            int cacheingNum = 0;
+            long cacheSize = 0;
+            List<DaoCatalogueBean> daoCatalogueBeans = DaoUtil.getInstance().queryCatalogueListByQueryBuilder(daoMicroLessonBean.getId());
+            for (DaoCatalogueBean daoCatalogueBean : daoCatalogueBeans) {
+                List<DaoVideoBean> daoVideoBeans1 = DaoUtil.getInstance().queryVideoListByQueryBuilder(daoCatalogueBean.getId());
+                cacheNum += daoVideoBeans1.size();
+                for (DaoVideoBean daoVideoBean : daoVideoBeans1) {
+                    if (DaoVideoBean.DOWNLOAD_OVER.equals(daoVideoBean.getDownloadStatus())) {
+                        cacheingNum += 1;
+                        cacheSize += Long.parseLong(daoVideoBean.getSize());
+                    }
+                }
+            }
+            daoMicroLessonBean.setCacheNum("已缓存" + cacheingNum + "节/共" + cacheNum + "节");
+            daoMicroLessonBean.setCacheSize(FileUtils.getPrintSize(cacheSize));
+
+        }
+
     }
 }
