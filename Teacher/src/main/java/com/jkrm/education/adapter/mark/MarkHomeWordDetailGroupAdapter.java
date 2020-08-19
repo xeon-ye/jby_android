25ca83f6a434f768c2527b1152ff5e3895a05596
chexiangqian <2439836687@qq.com>
2020-08-19 19:04:06 +0800

    分类

--- Teacher/src/main/java/com/jkrm/education/adapter/mark/MarkHomeWordDetailGroupAdapter.java
+++ Teacher/src/main/java/com/jkrm/education/adapter/mark/MarkHomeWordDetailGroupAdapter.java
@@ -16,32 +16,33 @@ import java.util.List;
  * @CreateDate: 2020/8/19 18:25
  */
 
-public class MarkHomeWordDetailGroupAdapter extends BaseQuickAdapter<HomeworkDetailResultBean.GradQusetionBean, BaseViewHolder> {
-    private List<HomeworkDetailResultBean.GradQusetionBean> mList = new ArrayList<>();
+public class MarkHomeWordDetailGroupAdapter extends BaseQuickAdapter<List<HomeworkDetailResultBean.GradQusetionBean>, BaseViewHolder> {
+    private List<List<HomeworkDetailResultBean.GradQusetionBean>> mList = new ArrayList<>();
 
     public MarkHomeWordDetailGroupAdapter() {
         super(R.layout.adapter_homework_group_detail);
     }
 
-    @Override
-    protected void convert(BaseViewHolder helper, HomeworkDetailResultBean.GradQusetionBean item) {
 
-    }
-    public void addAllData(List<HomeworkDetailResultBean.GradQusetionBean> dataList) {
+    public void addAllData(List<List<HomeworkDetailResultBean.GradQusetionBean>> dataList) {
         this.mList = dataList;
         this.setNewData(mList);
         notifyDataSetChanged();
     }
 
     public void clearData() {
-        if(mList != null) {
+        if (mList != null) {
             int startPosition = 0;//hasHeader is 1
             int preSize = this.mList.size();
-            if(preSize > 0) {
+            if (preSize > 0) {
                 this.mList.clear();
                 notifyItemRangeRemoved(startPosition, preSize);
             }
         }
     }
 
+    @Override
+    protected void convert(BaseViewHolder helper, List<HomeworkDetailResultBean.GradQusetionBean> item) {
+
+    }
 }
